2c4719941934dfc86725891f8560d1cfd191ce0d
Toby Penner <tobypenner01@gmail.com>
2025-12-27 16:24:00 -0700

    Add grad clipping

--- offline/bc/train.py
+++ offline/bc/train.py
@@ -74,6 +74,7 @@ def train(config: BCConfig) -> None:
             predictions = model(features)
             loss = criterion(predictions, labels)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
 
             train_loss += loss.item() * len(labels)
