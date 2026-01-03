e7dfbc0a8efa285cba7a6ef8aa6816dbc286c5c7
Toby Penner <tobypenner01@gmail.com>
2026-01-02 23:58:40 -0700

    One eval at end

--- offline/config.py
+++ offline/config.py
@@ -61,10 +61,10 @@ class BCConfig:
     lr: float = 3e-4
     lr_min: float = 5e-6
     weight_decay: float = 1e-5
-    epochs: int = 50
+    epochs: int = 100
 
     # Evaluation
-    eval_every_n_epochs: int = 25
+    eval_every_n_epochs: int = 100
     eval_num_segments: int = 5000
 
     # Paths
